a31b4d37e9c95f33d52c90a9f0fa219e6e2e1d3e
freddster14 <freddycarrillo951@gmail.com>
2025-10-24 11:41:14 -0500

    Fix database queries to use 'posts' table instead of 'messages'; update modal form for password input with character limit indication

--- model/db/posts.js
+++ model/db/posts.js
@@ -2,7 +2,7 @@ const pool = require('./pool');
 
 exports.createPost = async (content) => {
   await pool.query(
-    'INSERT INTO messages (title, message, author_id, author_username) VALUES ($1,$2,$3,$4)',
+    'INSERT INTO posts (title, message, author_id, author_username) VALUES ($1,$2,$3,$4)',
     [
       content.title,
       content.message,
@@ -13,11 +13,11 @@ exports.createPost = async (content) => {
 };
 
 exports.getPosts = async (limit) => {
-  const { rows } = await pool.query('SELECT * FROM messages ORDER BY id DESC LIMIT $1 ', [limit]);
+  const { rows } = await pool.query('SELECT * FROM posts ORDER BY id DESC LIMIT $1 ', [limit]);
   return rows;
 };
 
 exports.getUserPosts = async (id) => {
-  const { rows } = await pool.query('SELECT * FROM messages WHERE author_id=$1', [id]);
+  const { rows } = await pool.query('SELECT * FROM posts WHERE author_id=$1', [id]);
   return rows;
 };
