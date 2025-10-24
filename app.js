bbf1980ebc93c3583cd8d6854a751a3fe23c7ad8
freddster14 <freddycarrillo951@gmail.com>
2025-10-24 12:15:27 -0500

    Add PostgeSQL session store for production

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('node:path');
 const passport = require('passport');
 const session = require('express-session');
+const PGSession = require('connect-pg-simple')(session);
+const pool = require('./model/db/pool');
 const dayjs = require('dayjs');
 const relativeTime = require('dayjs/plugin/relativeTime');
 const mainRouter = require('./routes/main');
@@ -19,11 +21,17 @@ app.set('view engine', 'ejs');
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
+  store: new PGSession({
+    pool,
+    tableName: 'session',
+    createTableIfMissing: true,
+  }),
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 5,
+    httpOnly: true,
   },
 }));
 app.use(passport.session());
